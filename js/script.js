91b5fb1c7d1f9c735f644940b93c2fc0b8a8a904
Student <example@example.com>
2019-10-22 10:57:11 -0700

    add funtional js buttons

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button#lightblue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("lightblue-background");
+  });
+ $("button#pink").click(function() {
+   $("body").removeClass();
+   $("body").addClass("pink-background");
+ })
+})
